8dda9e10544086fe28ce513b967a8b81eaa41518
JAC Entreprise <jacentrprise@gmail.com>
2016-06-28 16:34:53 +0200

    completer le profil des membres

---
+++ app/controllers/formulaire/FormulaireMembre.java
@@ -0,0 +1,7 @@
+package controllers.formulaire;
+
+/**
+ * Created by brick on 24/06/2016.
+ */
+public class FormulaireMembre {
+}
---
+++ app/controllers/membre/Formulaire.java
@@ -0,0 +1,68 @@
+package controllers.membre;
+
+import repository.MembreRepository;
+
+/**
+ * Created by brick on 06/06/2016.
+ */
+public class Formulaire {
+    private String email;
+    private String motDePasse;
+    private String confirmation;
+    private String adresse;
+    private String telephone;
+    private String siteweb;
+    private final MembreRepository repository;
+
+    public Formulaire() {
+        repository = MembreRepository.instance;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getConfirmation() {
+        return confirmation;
+    }
+
+    public void setConfirmation(String confirmation) {
+        this.confirmation = confirmation;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getSiteweb() {
+        return siteweb;
+    }
+
+    public void setSiteweb(String siteweb) {
+        this.siteweb = siteweb;
+    }
+}
